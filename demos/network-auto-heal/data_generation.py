3c18a89ec3a61ece3f6e7c00358e069a8c16a29d
anantg <anant1985@gmail.com>
2019-06-19 22:18:40 +0800

    TSDB ingestion enabled

--- demos/network-auto-heal/data_generation.py
+++ demos/network-auto-heal/data_generation.py
@@ -7,7 +7,8 @@ import random
 import time
 import requests
 
-NUCLIO_FUNCTION_URL = 'http://10.80.1.55:32736'
+KV_NUCLIO_FUNCTION_URL = 'http://10.80.1.55:32736'
+TSDB_NUCLIO_FUNCTION_URL = 'http://10.80.1.55:30031'
 logger = None
 
 
@@ -37,11 +38,50 @@ def handle_exception(request, exception):
 
 # ingest record by calling the nuclio function
 def ingest_record(rec):
+    str_rec = ','.join(str(e) for e in rec)
     try:
-        response = requests.request("POST", NUCLIO_FUNCTION_URL, data=rec, headers={'Content-Type': 'text/plain'})
-        print(response.request.data)
+        response = requests.request("POST", KV_NUCLIO_FUNCTION_URL, data=str_rec, headers={'Content-Type': 'text/plain'})
         if response.status_code != 200:
             logger.error("nuclio function call failed with status code %s for record %s", response.status_code,
+                         response.request.body)
+        else:
+            create_tsdb_record(rec)
+    except Exception as e:
+        logger.error(str(e))
+    pass
+
+
+# create payload with multiple metrices for ingestion via TSDB nuclio function
+def create_tsdb_record(rec):
+    metric_util_per = "utilization"
+    metric_pkt_rate = "packet_rate"
+    metric_latency = "latency"
+
+    sample_time = str(rec[18]*1000)
+    latency_value = str(rec[14])
+    util_value = str(rec[11])
+    pkt_rate_value = str(rec[13])
+
+    label_pe_name = "source_router"
+    label_dest_pe_name = "destination_router"
+    label_link_status = "link_status"
+    label_link_id = "link_id"
+    label_direction = "direction_of_traffic"
+    label_max_bw = "maximum_bw"
+
+    label_pe_name_value = str(rec[5])
+    label_dest_pe_name_value = str(rec[19])
+    label_link_status_value = str(rec[12])
+    label_link_id_value = str(rec[1])
+    label_direction_value = str(rec[2])
+    label_max_bw_value = str(rec[8])
+
+    tsdb_payload = "[\n\t{\n\t\t\t\"metric\": \""+metric_util_per+"\",\n\t\t\t\"labels\": {\n\t\t\t\t\""+label_pe_name+"\": \""+label_pe_name_value+"\",\n\t\t\t\t\""+label_dest_pe_name+"\": \""+label_dest_pe_name_value+"\",\n\t\t\t\t\""+label_link_status+"\": \""+label_link_status_value+"\",\n\t\t\t\t\""+label_link_id+"\": \""+label_link_id_value+"\",\n\t\t\t\t\""+label_direction+"\": \""+label_direction_value+"\",\n\t\t\t\t\""+label_max_bw+"\": \""+label_max_bw_value+"\"\n\t\t\t},\n\t\t\t\"samples\": [\n\t\t\t\t{\n\t\t\t\t\t\"t\": \""+sample_time+"\",\n\t\t\t\t\t\"v\": {\n\t\t\t\t\t\t\"N\": "+util_value+"\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t]\n\t},\n\t{\n\t\t\t\"metric\": \""+metric_pkt_rate+"\",\n\t\t\t\"labels\": {\n\t\t\t\t\""+label_pe_name+"\": \""+label_pe_name_value+"\",\n\t\t\t\t\""+label_dest_pe_name+"\": \""+label_dest_pe_name_value+"\",\n\t\t\t\t\""+label_link_status+"\": \""+label_link_status_value+"\",\n\t\t\t\t\""+label_link_id+"\": \""+label_link_id_value+"\",\n\t\t\t\t\""+label_direction+"\": \""+label_direction_value+"\",\n\t\t\t\t\""+label_max_bw+"\": \""+label_max_bw_value+"\"\n\t\t\t},\n\t\t\t\"samples\": [\n\t\t\t\t{\n\t\t\t\t\t\"t\": \""+sample_time+"\",\n\t\t\t\t\t\"v\": {\n\t\t\t\t\t\t\"N\": "+pkt_rate_value+"\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t]\n\t},\n\t{\n\t\t\t\"metric\": \""+metric_latency+"\",\n\t\t\t\"labels\": {\n\t\t\t\t\""+label_pe_name+"\": \""+label_pe_name_value+"\",\n\t\t\t\t\""+label_dest_pe_name+"\": \""+label_dest_pe_name_value+"\",\n\t\t\t\t\""+label_link_status+"\": \""+label_link_status_value+"\",\n\t\t\t\t\""+label_link_id+"\": \""+label_link_id_value+"\",\n\t\t\t\t\""+label_direction+"\": \""+label_direction_value+"\",\n\t\t\t\t\""+label_max_bw+"\": \""+label_max_bw_value+"\"\n\t\t\t},\n\t\t\t\"samples\": [\n\t\t\t\t{\n\t\t\t\t\t\"t\": \""+sample_time+"\",\n\t\t\t\t\t\"v\": {\n\t\t\t\t\t\t\"N\": "+latency_value+"\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t]\n\t}\n]"
+    print(tsdb_payload)
+    try:
+        response = requests.request("POST", TSDB_NUCLIO_FUNCTION_URL, data=tsdb_payload, headers={'Content-Type': 'application/json'})
+        if response.status_code != 200:
+            logger.error("TSDB ingestion failed with status code %s for record %s", response.status_code,
                          response.request.body)
     except Exception as e:
         logger.error(str(e))
@@ -71,9 +111,7 @@ def create_records(rec):
 
     # historical record key
     rec[0] = rec[0].split("_")[0]+"_"+str(rec[18])
-    str_rec = ','.join(str(e) for e in rec)
-    ingest_record(str_rec)
-    #print(str_rec)
+    ingest_record(rec)
 
 
 def main():
@@ -85,6 +123,9 @@ def main():
                 for row in file_reader:
                     create_records(row)
             dataFile.close()
+
+            # sleeping for one second
+            time.sleep(1)
     else:
         logger.error("sample data file not present.existing program ...")
 
